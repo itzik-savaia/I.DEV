330991a34640145ce0bada0b619cf031df7ecc7d
itzikking <itzikking1@gmail.com>
2021-11-23 09:56:45 +0200

    Commit message

--- server/server.js
+++ server/server.js
@@ -5,6 +5,14 @@ const PORT = 8000;
 
 app.use(express.static(path.join(__dirname, "../client", "build")));
 
+app.all('*', (req, res) => res.redirect("/"));
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 })
+app.use(function (error, req, res, next) {
+    console.log('req =>', req);
+    console.log('error =>', error);
+    console.log('res =>', res);
+});
+
